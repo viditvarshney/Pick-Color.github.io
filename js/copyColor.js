168049133d21e5a83405a8f1f575a4b026b64264
Shivam Sharma <38954435+Shivamdot@users.noreply.github.com>
2019-01-15 13:32:47 +0530

    Resolved multi click problem
    When a user was clicking a color box more than one time then the message ('Copied!') was getting copied to clipboard.

--- js/copyColor.js
+++ js/copyColor.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
     $('.color-box').click(function() {
 
-        //Temp. input to copy color to clip Board 
-        let temp1 = $("<input>");
-        $("body").append(temp1);
-        temp1.val($(this).find('.hex-number').text()).select();
-        document.execCommand("copy");
-        temp1.remove();
-        
+        //Temp. input to copy color to clip Board
+        if($(this).find('.hex-number').text() !== 'Copied!') {
+            let temp1 = $("<input>");
+            $("body").append(temp1);
+            temp1.val($(this).find('.hex-number').text()).select();
+            document.execCommand("copy");
+            temp1.remove();
+        } 
+
         //Changing hexNumber to "Copied!"
-        let hexNumber = $(this).find('.hex-number').text();
+        let hexNumber;
+        if($(this).find('.hex-number').text() !== 'Copied!') {
+            hexNumber = $(this).find('.hex-number').text();
+        }
         $(this).find('.hex-number').text('Copied!');
         setTimeout(() => {
             $(this).find('.hex-number').text(hexNumber);
